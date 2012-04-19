8ed435dabd891d130b10ce31babbce17d4d15a87
Richard Clermont <rich10@vt.edu>
2012-04-19 15:03:16 -0400

    added AI.cpp, AI_main.cpp and Makefile_AI

--- AI.h
+++ AI.h
@@ -1,19 +1,8 @@
 #pragma once
 
-class AI {
-public:
-	AI();  // Constructer
+namespace BattleshipAI {
 
-	// Returns the size of the smallest ship, not already destroyed
-	int smallestShipLeft(/*stdin*/);
-
-	// Returns ship that needs to be finnished off or -1 if there aren't any
-	int unfinishedShip(/*stdin*/);
-
-	// Location of guess
-	location Guess();
-private:
-	// The guessing strategy
+	// The guessing strategies
 	enum {
 		spiral_out,
 		spiral_in,
@@ -21,7 +10,23 @@ private:
 		random_only_diag,
 		x_subdivide
 	};
-	int guessingStrat;
 
-	location prev_guess; // This might not be needed or might need to be a list
-};
+	class AI {
+	public:
+		AI(int guess_strat = random_only_diag);  // Constructer
+
+		// Returns the size of the smallest ship not already destroyed
+		int smallestShipLeft(/*stdin*/);
+
+		// Returns ship that needs to be finnished off or -1 if there aren't any
+		int unfinishedShip(/*stdin*/);
+
+		// Location of guess
+		location Guess();
+	private:
+		// stores which strategy is being used
+		int guessingStrat;
+
+		location prev_guess; // This might not be needed or might need to be a list
+	};
+}
---
+++ AI_main.cpp
@@ -0,0 +1,10 @@
+#include "AI.h"
+#include "location.h"
+#include "board.h"
+
+int main() {
+	// Initialize AI
+	BattleshipAI::AI AI_player;
+	
+	// input board status
+}
