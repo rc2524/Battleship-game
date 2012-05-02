0edc8499f0be3a91e3413bbcc777b302c86c1e9c
Tyler Roberts <trob52@vt.edu>
2012-05-02 05:53:33 -0400

    Make would not work with out these in main folder

---
+++ battlelocalwindow.h
@@ -0,0 +1,52 @@
+#ifndef BATTLELOCALWINDOW_H
+#define BATTLELOCALWINDOW_H
+
+#include <QDialog>
+#include <QTextBrowser>
+#include <iostream>
+#include "Board.h"
+#include "Ship.h"
+#include "Location.h"
+#include <stdlib.h>
+#include <time.h>
+
+using namespace std;
+
+static int localseed = time(NULL);
+
+namespace Ui {
+    class battleLocalWindow;
+}
+
+class battleLocalWindow : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit battleLocalWindow(QWidget *parent = 0);
+    ~battleLocalWindow();
+
+    void nextTurn();
+
+private slots:
+    void createRandomBoard();
+    void createCustomBoard();
+    void fire();
+
+private:
+    Ui::battleLocalWindow *ui;
+    int CurrentPlayer;
+    Board *Player1Board;
+    Board *Player2Board;
+    Board *EnemyBoard;
+    Board *PlayerBoard;
+    QString testOut;
+    void switchPlayer();
+    void printBoardText(Board *boardToPrint, QTextBrowser *textbox, bool showAll);
+    void gameOver();
+    bool checkGame(); //Check EnemyBoard
+    bool goodCoordinate(int row, int col);
+    void sunkShip();
+};
+
+#endif // BATTLELOCALWINDOW_H
